4b6a0e5050e1a2ab825d3eefcc3fca28a36c8868
ivteplo <iteplov7@gmail.com>
2021-09-10 23:16:26 +0300

    Allow downloading QR Code and changing it's theme

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import "./App.css"
 import Content from "./Content"
 import { ThemeProvider } from "./ThemeContext"
 
+const toCamelCase = (str) =>
+  str
+    .replace(/\-/g, " ")
+    .trim()
+    .split(" ")
+    .map((word, index) =>
+      index === 0
+        ? word
+        : word.substring(0, 1).toUpperCase() + word.substring(1)
+    )
+    .join("")
+
 function App() {
   const app = useRef()
 
@@ -21,10 +33,14 @@ function App() {
       "background",
       "background-darker",
       "foreground",
+      "qr-code-light-foreground",
+      "qr-code-light-background",
+      "qr-code-dark-foreground",
+      "qr-code-dark-background",
     ]
 
     return colors.reduce(
-      (prev, color) => ({ ...prev, [color]: getColor(color) }),
+      (prev, color) => ({ ...prev, [toCamelCase(color)]: getColor(color) }),
       {}
     )
   }
--- src/QRCode.js
+++ src/QRCode.js
@@ -1,11 +1,32 @@
 import QRCodeGenerator from "qrcode"
 import { ThemeContext } from "./ThemeContext"
-import { useRef, useEffect, useContext } from "react"
+import { useRef, useEffect, useContext, useState } from "react"
+import { ReactComponent as DownloadIcon } from "./icons/download.svg"
+import { ReactComponent as MoonIcon } from "./icons/moon.svg"
+import { ReactComponent as SunIcon } from "./icons/sun.svg"
 import "./QRCode.css"
 
 export function QRCode({ input }) {
   const canvas = useRef()
-  const { colors } = useContext(ThemeContext)
+  const { colors, theme: _theme } = useContext(ThemeContext)
+
+  const [theme, setTheme] = useState(_theme)
+
+  const download = () => {
+    if (!canvas.current) {
+      return
+    }
+
+    const link = document.createElement("a")
+    link.download = "QRCode.png"
+    link.href = canvas.current.toDataURL()
+    link.click()
+    link.remove()
+  }
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
 
   useEffect(() => {
     if (input) {
@@ -15,8 +36,13 @@ export function QRCode({ input }) {
         {
           width: 275,
           color: {
-            light: colors.background,
-            dark: colors.foreground,
+            light:
+              colors[
+                "qrCode" + (theme === "dark" ? "Dark" : "Light") + "Background"
+              ],
+            dark: colors[
+              "qrCode" + (theme === "dark" ? "Dark" : "Light") + "Foreground"
+            ],
           },
         },
         (error) => {
@@ -30,13 +56,47 @@ export function QRCode({ input }) {
         ?.getContext("2d")
         .clearRect(0, 0, canvas.current.width, canvas.current.height)
     }
-  }, [canvas, colors, input])
+  }, [canvas, colors, theme, input])
+
+  useEffect(() => {
+    setTheme(_theme)
+  }, [_theme])
 
   return (
-    <canvas ref={canvas} className="QR">
-      Sorry, your browser does not support HTML5. Please, update your browser in
-      order to use this website
-    </canvas>
+    <div className="QRCodeWrapper">
+      <canvas ref={canvas} className="QR">
+        Sorry, your browser does not support HTML5. Please, update your browser
+        in order to use this website
+      </canvas>
+      {input && (
+        <section className="QRCodeActions">
+          <button
+            onClick={toggleTheme}
+            type="button"
+            className="SubmitButton"
+            title={
+              "Toggle to " + (theme === "dark" ? "dark" : "light") + " theme"
+            }
+          >
+            {theme === "dark" ? (
+              <SunIcon strokeWidth="10" className="ButtonIcon" />
+            ) : (
+              <MoonIcon strokeWidth="10" className="ButtonIcon" />
+            )}
+            <span className="ScreenReaderOnly">Toggle theme</span>
+          </button>
+          <button
+            onClick={download}
+            type="button"
+            className="SubmitButton"
+            title="Download"
+          >
+            <DownloadIcon strokeWidth="10" className="ButtonIcon" />
+            <span className="ScreenReaderOnly">Download</span>
+          </button>
+        </section>
+      )}
+    </div>
   )
 }
 
